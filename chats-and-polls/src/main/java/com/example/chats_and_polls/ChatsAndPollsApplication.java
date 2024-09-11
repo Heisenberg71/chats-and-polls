52c1bbf38460f41cbd8a098bad00ba45cb92831d
Samin <samin99099@gmail.com>
2024-09-11 07:26:52 +0600

    build spring boot project outline

---
+++ chats-and-polls/src/main/java/com/example/chats_and_polls/ChatsAndPollsApplication.java
@@ -0,0 +1,13 @@
+package com.example.chats_and_polls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatsAndPollsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatsAndPollsApplication.class, args);
+	}
+
+}
---
+++ chats-and-polls/src/test/java/com/example/chats_and_polls/ChatsAndPollsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chats_and_polls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsAndPollsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
